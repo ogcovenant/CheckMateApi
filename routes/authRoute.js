b02349a78e99548ec0338d6bfb97b6fe78694ad2
Covenant Ogowale <justcovenant@outlook.com>
2024-04-29 15:38:52 +0100

    finished the login route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,9 +1,13 @@
 import express from "express"
 import createUser from "../controllers/users/createUser.js"
+import checkUser from "../controllers/users/checkUser.js"
 
 export const router = express.Router()
 
 router.route("/signup")
   .post(createUser)
+
+router.route("/login")
+  .post(checkUser)
 
 export default router
